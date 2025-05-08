8ace6cea7f101c1191f927b203efbb97bfc3538c
drdrdr333 <91769069+drdrdr333@users.noreply.github.com>
2025-05-08 11:53:48 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,38 +1,61 @@
-﻿namespace Fun
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine(Pig("Pig latin is cool"));
-            Console.WriteLine(Pig("Hello world !"));
-        }
+﻿//Int32
+const string promptId = "Please enter your ID: ";
+Console.Write(promptId);
 
-        public static string Pig(string str) {
-            string[] s = str.Split(" ");
-            string final = string.Empty;
-            foreach (string word in s) {
-                char first = ' ';
-                string newWord = string.Empty;
-                for (int i = 0; i < word.Length; i++)
-                {
-                    if (i == 0)
-                    {
-                        first = word[i];
-                        continue;
-                    }
-                    newWord += $"{word[i]}";
-                }
-                if (word == "!")
-                {
-                    final += $"{word} ";
-                    break;
-                }
-                newWord += $"{first}ay ";
-                final += $"{newWord}";
-            }
-            final.Remove(final.Length - 1);
-            return final;
-        }
-    }
+string? idString;
+idString = Console.ReadLine();
+int id;
+
+bool isIdInt = int.TryParse(idString, out id);
+Console.WriteLine("ID is an int? " + isIdInt + " ID: " + id);
+
+// Double
+const string promptDouble = "\nPlease enter the price of gas per gallon: ";
+Console.WriteLine(promptDouble);
+
+string? gasPrice;
+gasPrice = Console.ReadLine();
+double price;
+
+bool isPriceDouble = double.TryParse(gasPrice, out price);
+Console.WriteLine("Gas price is double? " + isPriceDouble + " Price/gallon: " + price);
+
+// DateTime
+const string promptDate = "Please enter todays date: ";
+Console.Write(promptDate);
+
+string? dateString;
+dateString = Console.ReadLine();
+DateTime dt;
+
+bool isDate = DateTime.TryParse(dateString, out dt);
+Console.WriteLine("User input is a date? " + isDate + " The date: " + dt);
+
+/*
+ * PART 2
+ */
+
+int x = 10;
+int y = x;
+
+Console.WriteLine("x = " + x + " y = " + y);
+Console.WriteLine("x == y? " + (x == y));
+
+y = 5;
+Console.WriteLine("x = " + x + " y = " + y);
+Console.WriteLine("x == y? " + (x == y));
+
+
+RefType r1 = new RefType() { a = 1, b = 2 };
+RefType r2 = new RefType() { a = 1, b = 2 };
+RefType r3 = r1;
+r1.a = 55;
+
+Console.WriteLine("r1 == r2? " + (r1 == r2));
+Console.WriteLine("r1 == r3? " + (r1 == r3));
+Console.WriteLine("r1.a = " + r1.a);
+Console.WriteLine("r3.a = " + r3.a);
+class RefType {
+    public int a;
+    public int b;
 }
